4c64b0d7c02bb5e41e5fbe05f831ef954b1bdedc
Boris <boris@randebrock.com>
2022-05-19 23:13:21 +0200

    tested userController test

---
+++ server/controllers/userControllers.test.js
@@ -0,0 +1,34 @@
+require("dotenv").config();
+const { userLogin } = require("./userControllers");
+
+jest.mock("../../db/models/User", () => ({
+  findOne: jest.fn().mockResolvedValue(true),
+}));
+
+jest.mock("bcrypt", () => ({
+  compare: jest.fn().mockResolvedValue(true),
+}));
+const statusCode = 200;
+const next = () => {};
+
+describe("Given the userControllers function", () => {
+  describe("When invoked with an existing username and password", () => {
+    const req = {
+      body: {
+        username: "bernat",
+        pasword: "bernat",
+      },
+    };
+
+    const res = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+
+    test("Then it should have been called called with the res.statusCode(200)", async () => {
+      await userLogin(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+  });
+});
