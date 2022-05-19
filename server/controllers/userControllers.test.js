7476aabf89d0333a17f80ffbe972585906db7fc5
Bernat Arcusa <bernat.arcusa@gmail.com>
2022-05-19 23:56:11 +0200

    Added test for userRegister in userControllers

--- server/controllers/userControllers.test.js
+++ server/controllers/userControllers.test.js
@@ -1,34 +1,55 @@
 require("dotenv").config();
-const { userLogin } = require("./userControllers");
+const { userLogin, userRegister } = require("./userControllers");
 
 jest.mock("../../db/models/User", () => ({
+  ...jest.requireActual("../../db/models/User"),
   findOne: jest.fn().mockResolvedValue(true),
+  create: jest.fn(),
 }));
 
 jest.mock("bcrypt", () => ({
+  ...jest.requireActual("bcrypt"),
   compare: jest.fn().mockResolvedValue(true),
+  hash: jest.fn().mockResolvedValue(),
 }));
-const statusCode = 200;
+
 const next = () => {};
 
-describe("Given the userControllers function", () => {
+const req = {
+  body: {
+    username: "bernat",
+    pasword: "bernat",
+  },
+};
+
+describe("Given the userLogin function", () => {
   describe("When invoked with an existing username and password", () => {
-    const req = {
-      body: {
-        username: "bernat",
-        pasword: "bernat",
-      },
-    };
+    test("Then it should have been called called with the status code 200", async () => {
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+      const statusCode = 200;
 
-    const res = {
-      status: jest.fn().mockReturnThis(),
-      json: jest.fn(),
-    };
-
-    test("Then it should have been called called with the res.statusCode(200)", async () => {
       await userLogin(req, res, next);
 
       expect(res.status).toHaveBeenCalledWith(statusCode);
+    });
+  });
+});
+
+describe("Given a userRegister function", () => {
+  describe("When invoked without an existing username and password", () => {
+    test("Then it should have been called called with the status code 201", async () => {
+      const createdStatusCode = 201;
+      const res = {
+        status: jest.fn().mockReturnThis(),
+        json: jest.fn(),
+      };
+
+      await userRegister(req, res, next);
+
+      expect(res.status).toHaveBeenCalledWith(createdStatusCode);
     });
   });
 });
